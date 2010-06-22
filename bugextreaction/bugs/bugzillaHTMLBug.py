a3e209a488b2b766de54f6428a49d21778eb4841
Adrian Schroeter <adrian.schroeter@googlemail.com>
2010-06-22 14:59:19 -0700

    fixed some bugs and its now running through on about 30k eclipse bug reports

--- bugextreaction/bugs/bugzillaHTMLBug.py
+++ bugextreaction/bugs/bugzillaHTMLBug.py
@@ -11,69 +11,145 @@ class BugzillaBugreport(ABug):
     self.addAttribute("bugid",bugid)
     self.namedict = {}
     self.namedict["Blocks"] = 'blocks'
-    
+    self.namedict["Summary"] = "shortdesc"
+    self.namedict['Priority'] = 'priority'
+    self.namedict['Status'] = 'status'
+    self.namedict['Assignee'] = 'assigned'
+    self.namedict['CC'] = 'cc'  
+    self.namedict['Component'] = 'component'
+    self.namedict['TargetMilestone'] = 'targetmilestone'
+    self.namedict['Product'] = 'product'
+    self.namedict['Resolution'] ='resolution'
+    self.namedict['OS'] = 'os'
+    self.namedict['Severity'] = 'severity'
+    self.namedict['Dependson'] = 'blocked-by'
+    self.namedict['Keywords'] = 'keywords'
+    self.namedict['Attachment'] = 'attachment'
+    self.namedict['Version'] = 'version'
+    self.namedict['Hardware'] = 'hardware'
+    self.namedict['Everconfirmed'] = 'Everconfirmed'
+    self.namedict['URL'] = 'URL'
+    self.namedict['QAContact'] = 'qacontact'
+    self.namedict['Whiteboard'] = 'whiteboard'
+    self.namedict['Flag'] = 'Flag'
+ 
+  def readChange(self,lines,i,cwho,cwhen):
+    if 'attachment.cgi?id=' in lines[i]:
+      if 'Flag' in lines[i+2]:
+        cwhat = self.namedict["Attachment"] + " - " + "flag" + " - " + lines[i].split('"')[1]
+        crem = ""
+        cadd = lines[i+6].split('>')[1]
+      elif 'mime type' in lines[i+2]:
+        cwhat = self.namedict["Attachment"] + " - " + "mimetype" + " - " + lines[i].split('"')[1]
+        crem = lines[i+4].split('>')[1]
+        cadd = lines[i+6].split('>')[1]
+      elif 'description' in lines[i+2]:
+        cwhat = self.namedict["Attachment"] + " - " + "description" + " - " + lines[i].split('"')[1]
+        crem = lines[i+4].replace("<td>",'').replace('\n','')
+        j = i+5
+        while '</td>' in lines[j]:
+          crem += '\n' + lines[j].replace("<td>",'').replace('\n','')
+          j += 1
+        cadd = lines[j+6].replace("<td>",'')
+        while '</td>' in lines[j]:
+          cadd += '\n' + lines[j].replace("<td>",'').replace('\n','')
+          j += 1
+        i += j-(i+6)
+      elif 'filename' in lines[i+2]:
+        cwhat = self.namedict["Attachment"] + " - " + "description" + " - " + lines[i].split('"')[1]
+        crem = lines[i+4].split('>')[1]
+        cadd = lines[i+6].split('>')[1]
+      else:
+        crem  = lines[i].split('"')[1]
+        cadd = ""
+        cwhat = self.namedict[lines[i+2].split(" is")[0].replace(" ","").replace("\n","")]
+      i += 2
+    else:
+      cwhat = self.namedict[lines[i].replace(" ","").replace("\n","")]
+      crem  = lines[i+2].split('>')[1]
+      if 'blocks' == cwhat:
+        if len(lines[i+4].split('id=')) > 1:
+          cadd = lines[i+4].split('id=')[1].split('"')[0]
+        else:
+          cadd = ""
+          crem = lines[i+2].split('id=')[1].split('"')[0]
+      else:
+        cadd  = lines[i+4].split('>')[1]
+    self.addChange( (cwho,cadd,crem,cwho,cwhen) )
+    return i
+
   def readChanges(self):
     lines = self.historylines
-    cwho  = None 
+    cwho  = None
     cwhen = None
     cwhat = None
     crem  = None
     cadd  = None
     i = 0
     while i < len(lines):
-      if '<td rowspan="1" valign="top">' in lines[i]:
+      if '<td rowspan="' in lines[i]:
         cwho  = lines[i].split('>')[1]
-        cwhen = lines[i+1].split('>')[1]
-        cwhat = self.namedict[lines[i+4].replace(" ","")]
-        crem  = lines[i+6].split('>')[1]
-        if 'blocks' == cwhat:
-          cadd = lines[i+8].split('id=')[1].split('"')[0]
+        cwhen = lines[i+2].split('>')[1]
+        if int(lines[i].split('"')[1]) > 1:
+          i += 3
+          while not ( ' </tr>' in lines[i+1] or ' </tr>' in lines[i+2]):
+            i += 2
+            i = self.readChange(lines,i,cwho,cwhen)
+            i += 5
         else:
-          cadd  = lines[i+8].split('>')[1]
-        self.addChange( (cwho,cadd,crem,cwho,cwhen) )
-    print "to implement"
+          i += 5
+          i = self.readChange(lines,i,cwho,cwhen)
+          i += 3
+      i += 1
  
   def readComment(self,i,lines):
     if '<span class="bz_comment_user">' in lines[i] and not 'Description' in lines[i-3]:
       date = lines[i+8]
-      commenter = lines[i].split('>')[3].split('<')[0]
+      commenter = lines[i].replace('<span class="fn">',"").split('>')[2].split('<')[0]
       comment = []
       j = i + 14
       while not "</div>" in lines[j]:
         comment.append(lines[j])
         j+=1
       self.addComment((date,commenter,comment))
-      self.readChanges(j,lines)
     
   def readAttachments(self,i,lines):
-    if 'attachment.cgi?id=' in lines[i] and not 'Details' in lines[i] and not 'Add an attachment' in lines[i]:
-      cfile = lines[i+5] + " " + lines[i+6].repalce(" ","")
+    if 'attachment.cgi?id=' in lines[i] and 'Details' in lines[i] and not '&gt;' in lines[i] and not 'bz_comment_text' in lines[i] and not 'Diff' in lines[i] and not 'Details' in lines[i] and not 'Add an attachment' in lines[i]:
+      cfile = lines[i+5] + " " + lines[i+6].replace(" ","")
       cdesc = ""
-      clink = lines[i].split('"')[4]
+      clink = lines[i].split('"')[3]
       cdev  = lines[i+11].split('>')[2].split('<')[0]
       cdate = lines[i+10].split('>')[1].split('<')[0]
       self.addAttachment( (cfile,cdesc,clink,cdev,cdate) )
-    
+  
   def readCreator(self,i,lines):
     if '<b>Reported</b>' in lines[i]:
-      self.addAttribute("creator"     , lines[i+2].split('>')[3].split('<')[0])
+      if len(lines[i+2].split('>')) > 3:
+        self.addAttribute("creator"     , lines[i+2].split('>')[3].split('<')[0])
+      else:
+        self.addAttribute("creator"     , lines[i+2].split('>')[2].replace("\n",""))
       self.addAttribute("creationDate", lines[i+2].split('>')[1].split('<')[0].replace('by',''))
 
   def readAssigned(self,i,lines):
-    if 'Assigned To' in lines[i]:
-      self.addAttribute("assigned", lines[i+2].split('>')[3].split('<')[0])
+    if 'page.cgi?id=fields.html#assigned_to' in lines[i]:
+      if len(lines[i+2].split('>')) > 3:
+        self.addAttribute("assigned", lines[i+2].split('>')[3].split('<')[0])
+      else:
+        self.printl(lines,i-2,i+4)
+        self.addAttribute("assigned", lines[i+2].split('>')[2].split('<')[0])
 
   def readCCs(self,i,lines):
     if '<b>CC List</b>' in lines[i]:
       self.addAttribute("cc",[])
       j = i+10
-      while not 'option' in lines[j]:
+      while 'option' in lines[j]:
         self.addAttribute("cc",lines[j].split('"')[1])
         j += 1    
 
   def readStatus(self,i,lines):
     if '<span id="static_bug_status">' in lines[i]:
-      self.addAttribute("status",lines[i].split('>')[1]+" "+lines[i+1].replace(" ",""))
+      self.addAttribute("resolution",lines[i+1].replace(" ",""))
+      self.addAttribute("status",lines[i].split('>')[1])
 
   def readProduct(self,i,lines):
     if '"field_container_product"' in lines[i]:
@@ -89,11 +165,13 @@ class BugzillaBugreport(ABug):
 
   def readPlatform(self,i,lines):
     if '<label for="rep_platform"' in lines[i]:
-      self.addAttribute("platform",lines[i+2].split('>')+" "+lines[i+3].replace(" ",""))
+      self.addAttribute("hardware",lines[i+2].split('>')[1])
+      self.addAttribute("os",lines[i+3].replace(" ",""))
 
   def readImportance(self,i,lines):
     if '<label for="priority"' in lines[i]:
-      self.addAttribute("importance",lines[i+3].split('>')[1]+" "+lines[i+4].replace(" ",""))
+      self.addAttribute("priority",lines[i+3].split('>')[1])
+      self.addAttribute("severity",lines[i+4].replace(" ",""))
 
   def readTargetMilestone(self,i,lines):
     if 'label for="target_milestone"' in lines[i]:
@@ -127,20 +205,25 @@ class BugzillaBugreport(ABug):
         self.addAttribute("blocks",t.split("id=")[1].split('"')[0])
 
   def readQAContact(self,i,lines):
-    if 'for="qa_contact' in lines[i] and '<span class="' in lines[i+2]:
-      self.addAttribute("qucontact",lines[i+2].split('>')[3].split('<')[0])
+    if 'for="qa_contact' in lines[i] and len(lines[i+2].split('<')) > 3:     
+      self.addAttribute("qacontact",lines[i+2].split('>')[3].split('<')[0])
 
   def readWhiteboard(self,i,lines):
     if 'for="status_whiteboard' in lines[i] and '<span title="' in lines[i+2]:
-      self.addAttribute("whiteboard",lines[i+2].split('"')[1]
+      self.addAttribute("whiteboard",lines[i+2].split('"')[1])
 
   def readKeywords(self,i,lines):
     if 'for="keywords"' in lines[i]:
       if len(lines[i+2].split('>')[1]) > 4:
         self.addAttribute("keywords",lines[i+2].split('>')[1])
 
+  def printl(self,lines,a,e):
+    import sys
+    while a < e:
+      sys.stderr.write(str(a) + " " + lines[a].replace("\n","") + '\n')
+      a += 1 
+
   def read(self):
-    print "need to update"
     i = 0
     while i < len(self.lines):
       self.readCreator(i,self.lines)
@@ -159,23 +242,11 @@ class BugzillaBugreport(ABug):
       self.readBlocks(i,self.lines)
       self.readComment(i,self.lines)
       self.readAttachments(i,self.lines)
-      self.readKeyWords(i,self.lines)
+      self.readKeywords(i,self.lines)
       self.readWhiteboard(i,self.lines)
       self.readQAContact(i,self.lines)
       i += 1
     self.readChanges()
       
   def completeChanges(self):
-    print "need to implement"
-    c = {}
-    c['shortdesc'] = self.attributes['shortdesc']
-    c['status'] = self.attributes['status']
-    c['assigned'] = self.attributes['assigned']
-    i = 0
-    while i < len(self.changes):
-      change = self.changes[i]
-      if change[0] == 'shortdesc' or change[0] == 'status' or change[0] == 'assigned':
-        t = change[2]
-        self.changes[i] = (change[0],c[change[0]],change[2],change[3],change[4])
-        c[change[0]] = t
-      i += 1
+    return True
